d654335b915f52fb276b422fd9030f92cb6553ca
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2021-01-15 17:34:00 +0100

    cart redux added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className='header'>
             <Link className='logo-container' to='/'>
@@ -29,15 +29,16 @@ const Header = ({ currentUser }) => {
                 }
                 <CartIcon/>
             </div>
-            <CartDropdown/>
+            { hidden ? null : <CartDropdown/> }
         </div>
 
     )
 }
 
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: { hidden }})=> ({
+    currentUser, 
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
